89c2bd2f294efebe5302e9fdca4411f05adc8980
Saatvik <saatvikaghuvanshi123@gmail.com>
2026-03-07 00:06:23 +0530

    fix: final manual config update

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyCfcKe3L9ILLITQcUpacBATGuG88Cs1id4",
   authDomain: "the-archive-project-f2ab4.firebaseapp.com",
+  // This is the URL that will stop the crash!
+  databaseURL: "https://the-archive-project-f2ab4-default-rtdb.firebaseio.com",
   projectId: "the-archive-project-f2ab4",
   storageBucket: "the-archive-project-f2ab4.firebasestorage.app",
   messagingSenderId: "198774506116",
-  appId: "1:198774506116:web:a90f65dd5930c43bd478d1",
-  databaseURL: "https://the-archive-project-f2ab4-default-rtdb.firebaseio.com/"
+  appId: "1:198774506116:web:a90f65dd5930c43bd478d1"
 };
-
 const app = initializeApp(firebaseConfig);
 
 // 2. Export db as the Realtime Database instance
